1e2f187282dcd945609c6b957e908fc0c9b95842
majczel23000 <michal.razny23@gmail.com>
2019-03-07 18:49:30 +0100

    feat(users): get all users method (API)

--- BACKEND/controllers/userController.js
+++ BACKEND/controllers/userController.js
@@ -3,6 +3,27 @@ import User from '../models/userModel.js';
 let errorResponse = require('../models/errorResponseModel').error;
 let successResponse = require('../models/successResponseModel').success;
 
+// Create new user and return user node
+exports.createUser = (req, res) => {
+    User.findOne({email: req.body.email}, (error, user) => {
+        if (error) {
+            res.send(error);
+        }
+        if (user) {
+            res.status(409).json(errorResponse(409, 'User with specified email already exists'));
+        } else {
+            const newUser = new User(req.body);
+            newUser.save((err, user2) => {
+                if (err) {
+                    res.send(err);
+                }
+                
+                res.status(200).json(successResponse(200, 'User successfully created', user2));
+            })
+        }
+    })    
+}
+
 exports.getUserById = (req, res) => {
     user.findById(req.body.id, (err, user) => {
         if (err) {
@@ -13,36 +34,15 @@ exports.getUserById = (req, res) => {
     });
 };
 
+// Return all user nodes
 exports.getAllUsers = (req, res) => {
-    user.find({}, (err, users) => {
+    User.find({}, (err, users) => {
         if (err) {
             res.send(err);
         }
 
-        res.json(users);
+        res.status(200).json(successResponse(200, 'Users successfully fetched', users));
     })
-}
-
-// Create new user
-// required: firstName, lastName, username, password, email
-exports.createUser = (req, res) => {
-    User.findOne({email: req.body.email}, (error, user) => {
-        if (error) {
-            res.send(error);
-        }
-        if (user) {
-            res.status(409).json(errorResponse(409, 'User with specified email already exists'));
-        } else {
-            const newUser = new User(req.body);
-            newUser.save((err, user3) => {
-                if (err) {
-                    res.send(err);
-                }
-                
-                res.status(200).json(successResponse(200, 'User successfully created', user3));
-            })
-        }
-    })    
 }
 
 exports.updateUser = (req, res) => {
