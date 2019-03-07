85324570ee45092ee6b90c4881a47ea2da4550ea
majczel23000 <michal.razny23@gmail.com>
2019-03-07 23:57:18 +0100

    feat(users): implement all basic methods (API)

--- BACKEND/controllers/userController.js
+++ BACKEND/controllers/userController.js
@@ -8,61 +8,68 @@ exports.createUser = (req, res) => {
     User.findOne({email: req.body.email}, (error, user) => {
         if (error) {
             res.send(error);
-        }
-        if (user) {
+        } else if (user) {
             res.status(409).json(errorResponse(409, 'User with specified email already exists'));
         } else {
             const newUser = new User(req.body);
             newUser.save((err, user2) => {
                 if (err) {
                     res.send(err);
+                } else {
+                    res.status(200).json(successResponse(200, 'User successfully created', user2));
                 }
-                
-                res.status(200).json(successResponse(200, 'User successfully created', user2));
             })
         }
     })    
 }
-
-exports.getUserById = (req, res) => {
-    user.findById(req.body.id, (err, user) => {
-        if (err) {
-            res.send(err);
-        }
-
-        res.json(user);
-    });
-};
 
 // Return all user nodes
 exports.getAllUsers = (req, res) => {
     User.find({}, (err, users) => {
         if (err) {
             res.send(err);
+        } else {
+            res.status(200).json(successResponse(200, 'Users successfully fetched', users));
         }
-
-        res.status(200).json(successResponse(200, 'Users successfully fetched', users));
     })
 }
 
-exports.updateUser = (req, res) => {
-    user.findOneAndUpdate({_id: req.body.id}, req.body, (err, user) => {
+// Return user with specified id
+exports.getUserById = (req, res) => {
+    User.findById(req.params.id, (err, user) => {
         if (err) {
             res.send(err);
-        }
-
-        res.json(user);
+        } else if (user) {
+            res.status(200).json(successResponse(200, 'User successfully fetched', user));
+        } else {
+            res.status(404).json(errorResponse(404, 'User with specified id not found'));
+        }  
     });
 };
 
-exports.deleteUser = (req, res) => {
-    user.remove({_id: req.body.id}, (err) => {
+// Update user and return updated user node
+exports.updateUser = (req, res) => {
+    req.body.updatedAt = new Date;
+    User.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, user) => {
         if (err) {
             res.send(err);
+        } else if (user) {
+            res.status(200).json(successResponse(200, 'User successfully updated', user));
+        } else {
+            res.status(404).json(errorResponse(404, 'User with specified id not found'));
         }
+    })
+};
 
-        res.json({
-            message: `user ${req.body.id} successfully deleted`
-        });
-    });
+// Remove user from database
+exports.deleteUser = (req, res) => {
+    User.findByIdAndRemove(req.params.id, (err, user) => {
+        if (err) {
+            res.send(err);
+        } else if (user) {
+            res.status(200).json(successResponse(200, 'User successfully removed', user));
+        } else {
+            res.status(404).json(errorResponse(404, 'User with specified id not found'));
+        }
+    })
 };
