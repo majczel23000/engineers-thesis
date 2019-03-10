83e0d88f40d3754fa7260359e72810f74be1e12f
majczel23000 <michal.razny23@gmail.com>
2019-03-10 20:58:18 +0100

    feat(users): add all validation methods to one file, validate required fields for user (API)

--- BACKEND/controllers/userController.js
+++ BACKEND/controllers/userController.js
@@ -2,37 +2,50 @@ import mongoose from 'mongoose';
 import User from '../models/userModel.js';
 let errorResponse = require('../models/errorResponseModel').error;
 let successResponse = require('../models/successResponseModel').success;
-let validateEmail = require('../middlewares/validateEmail').validateEmial;
-let validateFields = require('../middlewares/validateFields').validateFields;
+let validateEmail = require('../middlewares/validators').validateEmail;
+let validateFields = require('../middlewares/validators').validateFields;
 
 // CREATE: Create new user and return user node
 exports.createUser = (req, res) => {
-    const result = validateFields(User.schema.obj, req.body);
-    if (result.status) {
-        User.findOne({email: req.body.email}, (error, user) => {
-            if (error) {
-                res.send(error);
-            } else if (user) {
-                res.status(409).json(errorResponse(409, 'User with specified email already exists'));
-            } else {
-                if(validateEmail(req.body.email)) {
-                    const newUser = new User(req.body);
-                    newUser.save((err, user2) => {
-                        if (err) {
-                            res.send(err);
-                        } else {
-                            res.status(200).json(successResponse(200, 'User successfully created', user2));
-                        }
-                    })
-                } else {
-                    res.status(409).send(errorResponse(409, 'Please enter valid email'));
-                }
-                
-            }
-        })
-    } else {
-        res.status(409).send(errorResponse(409, result.message));
+    const validateFieldsResult = validateFields(User.schema.obj, req.body);
+    // firstName, lastName, email, password
+    if (!validateFieldsResult.status) {
+        res.status(409).send(errorResponse(409, validateFieldsResult.message));
+        return;
     }
+    if (!validateEmail(req.body.email)) {
+        res.status(409).send(errorResponse(409, 'Please enter valid email'));
+        return;
+    }
+    if (!(/^[a-zA-Z]+$/.test(req.body.firstName))) {
+        res.status(409).send(errorResponse(409, 'First Name must contain only letters without spaces'));
+        return;
+    }
+    if (!(/^[a-zA-Z]+$/.test(req.body.lastName))) {
+        res.status(409).send(errorResponse(409, 'Last Name must contain only letters without spaces'));
+        return;
+    }
+    if (req.body.password.length < 6) {
+        res.status(409).send(errorResponse(409, 'Password must have at least 6 characters'));
+        return;
+    }
+
+    User.findOne({email: req.body.email}, (error, user) => {
+        if (error) {
+            res.send(error);
+        } else if (user) {
+            res.status(409).json(errorResponse(409, 'User with specified email already exists'));
+        } else {
+            const newUser = new User(req.body);
+            newUser.save((err, user2) => {
+                if (err) {
+                    res.send(err);
+                } else {
+                    res.status(200).json(successResponse(200, 'User successfully created', user2));
+                }
+            })            
+        }
+    })
 }
 
 // GET-ALL: Return all user nodes
@@ -64,6 +77,10 @@ exports.updateUser = (req, res) => {
     req.body.updatedAt = new Date;
     if (req.body.email)
         delete req.body.email;
+    if (req.body.createdAt)
+        delete req.body.createdAt;
+    if (req.body.updatedAt)
+        delete req.body.updatedAt;
     User.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, user) => {
         if (err) {
             res.send(err);
--- BACKEND/middlewares/validators.js
+++ BACKEND/middlewares/validators.js
@@ -1,4 +1,8 @@
 module.exports = {
+    validateEmail: function(email) {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    },
     validateFields: function(object, data) {
         let result = {
             message: '',
@@ -7,7 +11,7 @@ module.exports = {
         for (var key in object) {
             if (object.hasOwnProperty(key)) {
                 if (object[key].required && (data[key] === '' || data[key] === null || data[key] === undefined)) {
-                    result.message += 'Field ' + key + ' can`t be empty. ';
+                    result.message += 'Field ' + key + ' is required. ';
                     result.status = false;
                 }
             }
