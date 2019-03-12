19cd6a6e22ac58720b17d2cfc2f586a4cd084992
majczel23000 <michal.razny23@gmail.com>
2019-03-12 18:46:26 +0100

    feat(check-role-middleware): modify checking role, now works correct (API)

--- BACKEND/middlewares/checkRole.js
+++ BACKEND/middlewares/checkRole.js
@@ -4,14 +4,18 @@ module.exports = {
     checkRole: function(roleName){
         return (request, response, next) => {
             if (!request.user.roles.length) {
-                response.status(401).json(errorResponse(401, "You don't have any role"));
+                return response.status(401).json(errorResponse(401, "You don't have any role"));
             } else {
+                let status = false;
                 for (let i = 0; i < request.user.roles.length; i++) {
                     if (request.user.roles[i] === roleName) {
-                        next()
-                    } else {
-                        response.status(401).json(errorResponse(401, "You don't have " + roleName + "role"));
+                        status = true
                     }
+                }
+                if (!status) {
+                    return response.status(401).json(errorResponse(401, "You don't have " + roleName + " role"));
+                } else {
+                    next()
                 }
             }
             
