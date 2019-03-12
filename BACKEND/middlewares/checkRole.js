40c97e62e28b5afe62225dcdaad848248c7c9f6e
majczel23000 <michal.razny23@gmail.com>
2019-03-12 20:49:16 +0100

    fix(check-role-middleware): change no permission message response (API)

--- BACKEND/middlewares/checkRole.js
+++ BACKEND/middlewares/checkRole.js
@@ -4,7 +4,7 @@ module.exports = {
     checkRole: function(roleName){
         return (request, response, next) => {
             if (!request.user.roles.length) {
-                return response.status(401).json(errorResponse(401, "You don't have any role"));
+                return response.status(401).json(errorResponse(401, "You don't have permission to perform this action!"));
             } else {
                 let status = false;
                 for (let i = 0; i < request.user.roles.length; i++) {
@@ -13,7 +13,7 @@ module.exports = {
                     }
                 }
                 if (!status) {
-                    return response.status(401).json(errorResponse(401, "You don't have " + roleName + " role"));
+                    return response.status(401).json(errorResponse(401, "You don't have permission to perform this action!"));
                 } else {
                     next()
                 }
