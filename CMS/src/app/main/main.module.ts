2ad1c8d790cdc1d22aee137eee6ef3d1e866bdbb
majczel23000 <michal.razny23@gmail.com>
2019-03-21 16:46:44 +0100

    feat(login): add view for login component (FRONT)

--- CMS/src/app/main/main.module.ts
+++ CMS/src/app/main/main.module.ts
@@ -4,12 +4,19 @@ import { CommonModule } from '@angular/common';
 import { MainRoutingModule } from './main-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule, MatCardModule, MatButtonModule } from '@angular/material';
 
 @NgModule({
   declarations: [DashboardComponent, LoginComponent],
   imports: [
     CommonModule,
-    MainRoutingModule
+    MainRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
   ]
 })
 export class MainModule { }
